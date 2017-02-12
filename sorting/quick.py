4c5afb4a0f3008fa743fa79b4103a751b134a24f
Hunter John Larco <admin@larcolabs.com>
2017-02-11 18:35:51 -0800

    Add quicksort inplace

--- sorting/quick.py
+++ sorting/quick.py
@@ -1,4 +1,25 @@
 # TODO(hunterlarco) Preform this in-place.
+def quick_inplace(arr, start=0, end=-1):
+  if len(arr) < 2: return
+  if end < 0: end += len(arr)
+  if end < 0: raise IndexError('list index out of range')
+  if end - start < 2: return
+  pivot = arr[start]
+  i = start + 1
+  j = end
+  while i < j:
+    while i <= j and arr[i] < pivot:
+      i += 1
+    while i <= j and arr[j] > pivot:
+      j -= 1
+    if i < j:
+      arr[i], arr[j] = arr[j], arr[i]
+      i += 1
+      j -= 1
+  arr[start], arr[j] = arr[j], arr[start]
+  if i - 1 > 0: quick_inplace(arr, start=start, end=i - 1)
+  quick_inplace(arr, start=j + 1, end=end)
+
 def quick(arr):
   if len(arr) < 2: return arr
 
@@ -18,12 +39,16 @@ def quick(arr):
   return quick(left) + pivots + quick(right)
 
 def main():
-  assert quick([]) == []
-  assert quick([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
-  assert quick([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
-  assert quick([5, 2, 3, 4, 1]) == [1, 2, 3, 4, 5]
-  assert quick([1, 5, 2, 4, 3]) == [1, 2, 3, 4, 5]
+  test([], [])
+  test([1, 2, 3, 4, 5], range(1, 6))
+  test([5, 4, 3, 2, 1], range(1, 6))
+  test([5, 2, 3, 4, 1], range(1, 6))
+  test([1, 5, 2, 4, 3], range(1, 6))
   print "PASSED"
+
+def test(arr, expected):
+  quick_inplace(arr)
+  assert arr == expected
 
 if __name__ == '__main__':
   main()
