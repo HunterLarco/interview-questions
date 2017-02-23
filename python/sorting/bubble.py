7509a6cecee5895b4bb8f2ff37e9ec45ff197590
Hunter John Larco <admin@larcolabs.com>
2017-02-22 22:26:23 -0800

    Add slicing to Bubble Sort

--- python/sorting/bubble.py
+++ python/sorting/bubble.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Bubble Sort implementation."""
 
-def bubble_sort(arr):
+def bubble_sort(arr, section=slice(None)):
   """Executes a bubble sort in-place.
 
   To learn about bubble sort, visit https://en.wikipedia.org/wiki/Bubble_sort
@@ -9,17 +9,20 @@ def bubble_sort(arr):
   Args:
       arr (list of any comparable type): The list that will be sorted via
           bubble sort.
+      section (:obj:`slice`, optional): Determines the section of the provided
+          list that will be sorted. Defaults to the entire array.
   """
+  step = section.step if not section.step is None else 1
   while True:
     preformed_swap = False
-    for i in range(len(arr) - 1):
-      if arr[i] > arr[i + 1]:
+    for i in range(*section.indices(len(arr) - step)):
+      if arr[i] > arr[i + step]:
         preformed_swap = True
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        arr[i], arr[i + step] = arr[i + step], arr[i]
     if not preformed_swap:
       break
 
-def bubble_sorted(arr):
+def bubble_sorted(arr, section=slice(None)):
   """Executes a bubble sort out of place akin to python's "sorted" builtin.
 
   To learn about bubble sort, visit https://en.wikipedia.org/wiki/Bubble_sort
@@ -27,10 +30,12 @@ def bubble_sorted(arr):
   Args:
       arr (list of any comparable type): The list that will be sorted via
           bubble sort.
+      section (:obj:`slice`, optional): Determines the section of the provided
+          list that will be sorted. Defaults to the entire array.
 
   Returns:
-      list of objects: A sorted copy of the parameter `arr`.
+      list of objects: A sorted copy of the parameter ``arr``.
   """
-  arr_copy = arr[:]
+  arr_copy = arr[section]
   bubble_sort(arr_copy)
   return arr_copy
--- python/tests/sorting/bubble_sort_test.py
+++ python/tests/sorting/bubble_sort_test.py
@@ -26,5 +26,39 @@ class BubbleSortTest(custom_test_cases.SortingTestCase):
     shuffled_list = self.create_shuffled_list(100)
     self.assertEqual(bubble.bubble_sorted(shuffled_list), sorted(shuffled_list))
 
+  def test_emptyList_withSection(self):
+    self.assertEqual(bubble.bubble_sorted([], section=slice(3, 5)), [])
+
+  def test_listWithOneElement_withSection_included(self):
+    self.assertEqual(bubble.bubble_sorted([1], section=slice(0, 5)), [1])
+
+  def test_listWithOneElement_withSection_excluded(self):
+    self.assertEqual(bubble.bubble_sorted([1], section=slice(3, 5)), [])
+
+  def test_reversedList_withSection_outOfPlace(self):
+    reversed_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    self.assertEqual(
+        bubble.bubble_sorted(reversed_list, section=slice(2, -2)),
+        [2, 3, 4, 5, 6, 7])
+    self.assertEqual(
+        bubble.bubble_sorted(reversed_list, section=slice(-2)),
+        [2, 3, 4, 5, 6, 7, 8, 9])
+    self.assertEqual(
+        bubble.bubble_sorted(reversed_list, section=slice(None, -2, 2)),
+        [3, 5, 7, 9])
+
+  def test_reversedList_withSection_inPlace(self):
+    reversed_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    bubble.bubble_sort(reversed_list, section=slice(2, -2))
+    self.assertEqual(reversed_list, [9, 8, 2, 3, 4, 5, 6, 7, 1, 0])
+
+    reversed_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    bubble.bubble_sort(reversed_list, section=slice(-2))
+    self.assertEqual(reversed_list, [2, 3, 4, 5, 6, 7, 8, 9, 1, 0])
+
+    reversed_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    bubble.bubble_sort(reversed_list, section=slice(None, -2, 2))
+    self.assertEqual(reversed_list, [3, 8, 5, 6, 7, 4, 9, 2, 1, 0])
+
 if __name__ == '__main__':
   unittest.main()
