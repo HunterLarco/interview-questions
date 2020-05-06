f0c3bbc62e20b303eefbed1c90e19a377f156667
Hunter Larco <hunter@larcolabs.com>
2020-05-06 20:10:49 +0000

    count triplets passed

---
+++ python/hackerrank/count_triplets.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+def countTriplets(arr, r):
+  ahead_count = Counter(arr)
+  behind_count = Counter()
+
+  count = 0
+  for value in arr:
+    ahead_count.subtract([value])
+    count += behind_count[value / r] * ahead_count[value * r]
+    behind_count.update([value])
+  return count
+
+
+def main():
+  print(countTriplets([1] * 100000, 1))
+
+
+if __name__ == '__main__':
+  main()
